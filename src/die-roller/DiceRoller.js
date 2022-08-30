350d6e2356a87bff13a9c2ec593ea51ad76ca6d8
CuriosityBard <curiositybard@gmail.com>
2022-08-30 08:45:39 -0700

    Add removal of dice and modifiers to die roller

--- src/die-roller/DiceRoller.js
+++ src/die-roller/DiceRoller.js
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
-import { rollMultiple, totalDice } from "./roller.js";
+import { rollMultiple, dropLowest, dropHighest, totalDice } from "./roller.js";
 import Dice from "./Dice.js";
 
 export function DiceRoller() {
     // dice data
     const [dice, setDice] = useState([]);
+    const [diceDropped, setDiceDropped] = useState([]);
     // input from user
     const [howManyDice, setHowManyDice] = useState(0);
     const [howManySides, setHowManySides] = useState(0);
+    const [modifier, setModifier] = useState(0);
+    // dice removed from the roll
+    const [removedDice, setRemovedDice] = useState(0);     // -1 means remove lowest, 1 means remove highest
+    const [numDiceToRemove, setNumDiceToRemove] = useState(0);
 
     // change dice
     function handleNumChange(event) {
@@ -16,9 +21,33 @@ export function DiceRoller() {
     function handleSidesChange(event) {
         setHowManySides(event.target.value);
     }
+    // change modifier/roll removal
+    function handleModifierChange(event) {
+        setModifier(event.target.value);
+    }
+    function handleRemovalChange(event) {
+        setRemovedDice(event.target.value);
+    }
+    function handleNumDiceToRemoveChange(event) {
+        setNumDiceToRemove(event.target.value);
+    }
     // update dice
     function getNewDice() {
-        setDice(rollMultiple(howManyDice, howManySides));
+        if (numDiceToRemove === 0) {
+            setDice(rollMultiple(howManyDice, howManySides));
+        } else {
+            if (parseInt(removedDice) === -1) {
+                let diceData = dropLowest(numDiceToRemove, rollMultiple(howManyDice, howManySides));
+                setDiceDropped(diceData.removed);
+                setDice(diceData.rolls);
+            } else if (parseInt(removedDice) === 1) {
+                let diceData = dropHighest(numDiceToRemove, rollMultiple(howManyDice, howManySides));
+                setDiceDropped(diceData.removed);
+                setDice(diceData.rolls);
+            } else {
+                console.log("There is a problem");
+            }
+        }
     }
 
     // must immediately update UI 
@@ -26,13 +55,28 @@ export function DiceRoller() {
 
     return (
         <div className="dice-roller">
-            <h2>Total: {totalDice(dice)}</h2>
+            <h2>Total: {totalDice(dice, parseInt(modifier))}</h2>
             <div className="dice-controls">
-                <p><input className="die-control" type="number" value={howManyDice} onChange={handleNumChange}></input>D<input className="die-control" type="number" value={howManySides} onChange={handleSidesChange}></input></p>
+                    <p>
+                        <input className="die-control" type="number" value={howManyDice} onChange={handleNumChange}></input>
+                        D
+                        <input className="die-control" type="number" value={howManySides} onChange={handleSidesChange}></input>
+                    </p>
+                    <p>Modifier: <input className="die-control" type="number" value={modifier} onChange={handleModifierChange}></input></p>
+                    <p>
+                        Remove <input className="die-control" type="number" value={numDiceToRemove} onChange={handleNumDiceToRemoveChange}></input>
+                        <input className="die-control" type="radio" name="remove-dice" value={-1} id="remove-lowest" onChange={handleRemovalChange}></input>
+                        <label htmlFor="remove-lowest">Lowest</label>
+                        <input className="die-control" type="radio" name="remove-dice" value={1} id="remove-highest" onChange={handleRemovalChange}></input>
+                        <label htmlFor="remove-highest">Highest </label>
+                        rolls
+                    </p>
                 <button className="die-control" onClick={getNewDice}>Roll!</button>
             </div>
             <h3>Rolls:</h3>
             <p><Dice allDice={dice} /></p>
+            <h3>Removed Rolls:</h3>
+            <p><Dice allDice={diceDropped} /></p>
         </div>
     );
 }
--- src/die-roller/roller.js
+++ src/die-roller/roller.js
@@ -13,13 +13,14 @@ function rollMultiple(n, x) {
 }
 
 // get total of a set of dice, given as an array arr
-function totalDice(arr) {
-    return arr.reduce((total, current) => total + current, 0);
+function totalDice(arr, modifier) {
+    return arr.reduce((total, current) => total + current, 0) + modifier;
 }
 
 // drop lowest n rolls from set arr
 function dropLowest(n, arr) {
     let returnArr = [...arr];
+    let removedArr = [];    // holds removed rolls 
     for (let i = 0; i < n; i++) {
         // find lowest value in array:
         let lowest = returnArr.reduce((final, current) => {
@@ -28,6 +29,8 @@ function dropLowest(n, arr) {
             }
             return current;
         }, returnArr[0]);
+        // add lowest value to removed array:
+        removedArr.push(lowest);
         //remove lowest value in array:
         let idx = returnArr.indexOf(lowest);
         if (idx === 0) {
@@ -38,12 +41,16 @@ function dropLowest(n, arr) {
             returnArr = [...returnArr.slice(0, idx), ...returnArr.slice(idx + 1)];
         }
     }
-    return returnArr;
+    return {
+        rolls: returnArr,
+        removed: removedArr
+    };
 }
 
 // drop highest n rolls from set arr
 function dropHighest(n, arr) {
     let returnArr = [...arr];
+    let removedArr = [];    // holds removed rolls 
     for (let i = 0; i < n; i++) {
         // find highest value in array:
         let highest = returnArr.reduce((final, current) => {
@@ -52,6 +59,8 @@ function dropHighest(n, arr) {
             }
             return current;
         }, returnArr[0]);
+        // add highest value to removed array:
+        removedArr.push(highest);
         //remove highest value in array:
         let idx = returnArr.indexOf(highest);
         if (idx === 0) {
@@ -62,7 +71,10 @@ function dropHighest(n, arr) {
             returnArr = [...returnArr.slice(0, idx), ...returnArr.slice(idx + 1)];
         }
     }
-    return returnArr;
+    return {
+        rolls: returnArr,
+        removed: removedArr
+    };
 }
 
 // modify roll with static value n, n can be negative or positive
